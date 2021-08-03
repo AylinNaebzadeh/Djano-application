d1b5de4475d8e88ca884707a26c3178bd627f8f3
aylin <a_naeb@comp.iust.ac.ir>
2021-08-04 00:50:11 +0430

    admin.py

--- Djangodb/Blog/admin.py
+++ Djangodb/Blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BookOrCourse
+from Blog.forms import ContactForm , NewUserForm
 
 # Register your models here.
+
+admin.site.register(BookOrCourse)
+admin.site.register(ContactForm)
+admin.site.register(NewUserForm)
